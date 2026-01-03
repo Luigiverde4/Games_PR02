9e88c317150213f6bfedbb559ab6ebeb284a0ff8
CGIMMAT <CGIMMAT@upv.edu.es>
2026-01-03 14:08:03 +0100

    Avanzado lógica de ingredientes

--- Assets/Scripts/IngredientManager.cs
+++ Assets/Scripts/IngredientManager.cs
@@ -4,54 +4,122 @@ using UnityEngine;
 
 public class IngredientManager : MonoBehaviour
 {
-    public gameObject ingredientPrefab;
+   public GameObject ingredientPrefab;
+   public Sprite ingredientSprite;
 
-    public Sprite pepperoni;
-    public Sprite mushroom;
-    public Sprite bacon;
-    public Sprite egg;
-    public Sprite onion;
-    public Sprite pepper;
-    public Sprite olive;
-    public Sprite pineapple;
-    public Sprite shrimp;
-    public Sprite cheese;
-    public Sprite anchovies;
-    public Sprite caper;
+   public float dragZ = -3f;
+   public Vector2 ingredientScale = new Vector2(1f,1f);
+   private GameObject currentIngredient;
+   private bool isDragging = false;
 
-    public float dragZ = -2f;
-
-    private gameObject currentIngredient;
-    private bool isDragging = false;
-    private string ingredientType;
-
-    void GetSprite(string type)
+   void OnMouseDown()
     {
-        switch (type)
-        {
-            case "Pepperoni": return pepperoni;
-            case "Mushroom": return mushroom;
-            case "Bacon": return bacon;
-            case "Egg": return egg;
-            case "Onion":return onion;
-            case "Pepper": return pepper;
-            case "Olive": return olive;
-            case "Pineapple": return pineapple;
-            case "Shrimp": return shrimp;
-            case "Cheese": return  cheese;
-        }
-    }
-
-    void SpawnIngredient()
-    {
-        ingredientType = gameObject.Tag;
-
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = dragZ;
         currentIngredient = Instantiate(ingredientPrefab, mousePos, Quaternion.identity);
 
         SpriteRenderer sr = currentIngredient.GetComponent<SpriteRenderer>();
-        sr.sprite = GetSprite(ingredientType);
+        sr.sprite = ingredientSprite;
+        currentIngredient.transform.localScale = new Vector3(ingredientScale.x, ingredientScale.y, 1f);
         isDragging = true;
-    } 
+    }
+
+    void PlaceIngredient()
+    {
+        isDragging = false;
+
+        Collider2D[] hits = Physics2D.OverlapPointAll(currentIngredient.transform.position);
+        Collider2D pizzaCollider = null;
+
+        foreach (var hit in hits)
+        {
+            if (hit.CompareTag("Pizza"))
+            {
+                pizzaCollider = hit;
+                break;
+            }
+        }
+
+        if (pizzaCollider == null)
+        {
+            Destroy(currentIngredient);
+            return;
+        }
+
+        PizzaManager pm = pizzaCollider.GetComponentInParent<PizzaManager>();
+        if (pm == null || pm.estado != "queso")
+        {
+            Destroy(currentIngredient);
+            return;
+        }
+
+        currentIngredient.transform.SetParent(pizzaCollider.transform);
+        Collider2D col = currentIngredient.GetComponent<Collider2D>();
+        if (col != null) col.enabled = false;
+
+        SpriteRenderer pizzaSR = pizzaCollider.GetComponent<SpriteRenderer>();
+        SpriteRenderer ingredientSR = currentIngredient.GetComponent<SpriteRenderer>();
+
+        if (pizzaSR != null && ingredientSR != null)
+        {
+            ingredientSR.sortingLayerID = pizzaSR.sortingLayerID;
+            ingredientSR.sortingOrder = pizzaSR.sortingOrder +1;
+        }
+
+        switch (gameObject.tag)
+        {
+            case "Pepperoni":
+                pm.pepperoni += 1;
+                break;
+            case "Mushroom":
+                pm.mushroom += 1;
+                break;
+            case "Bacon":
+                pm.bacon += 1;
+                break;
+            case "Egg":
+                pm.egg += 1;
+                break;
+            case "Olive":
+                pm.olive += 1;
+                break;
+            case "Onion":
+                pm.onion += 1;
+                break;
+            case "Pineapple":
+                pm.pineapple += 1;
+                break;
+            case "Pepper":
+                pm.pepper += 1;
+                break;
+            case "Shrimp":
+                pm.shrimp += 1;
+                break;
+            case "Cheese":
+                pm.cheese += 1;
+                break;
+            case "Anchovies":
+                pm.anchovies += 1;
+                break;
+            case "Caper":
+                pm.caper += 1;
+                break;
+        }
+
+        currentIngredient = null;
+    }
+
+    void Update()
+    {
+        if (!isDragging || currentIngredient == null) return;
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = dragZ;
+        currentIngredient.transform.position = mousePos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            PlaceIngredient();
+        }
+    }
 }
--- Assets/Scripts/PizzaManager.cs
+++ Assets/Scripts/PizzaManager.cs
@@ -10,12 +10,13 @@ public class PizzaManager : MonoBehaviour
     public int mushroom = 0;
     public int bacon = 0;
     public int egg = 0;
-    public int onion = 0;
-    public int pepper = 0;
     public int olive = 0;
+    public int onion = 0;
     public int pineapple = 0;
+    public int pepper = 0;
     public int shrimp = 0;
     public int cheese = 0;
+    public int anchovies = 0;
     public int caper = 0;
 
     public void setEstado(string nuevo)
